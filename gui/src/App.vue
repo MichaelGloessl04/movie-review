ae7988ee5dfcde3749cd31d194aa14866412d440
MichaelGloessl04 <gloess190117@sr.htlweiz.at>
2024-03-01 11:03:28 +0100

    removed div from app

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -6,17 +6,8 @@
     <h1>
       Movie Review
     </h1>
-    <div class="main-background">
-    </div>
   </body>
 </template>
 
 <style scoped>
-  .main-background {
-    background-color: rgb(34, 34, 34);
-    display: flex;
-    justify-content: center;
-    padding: 40px;
-    margin-bottom: 30px;
-  }
 </style>
